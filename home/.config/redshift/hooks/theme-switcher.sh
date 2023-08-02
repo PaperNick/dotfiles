ed1da4f998284b11b319803eae676a06817b648f
PaperNick <papernick@users.noreply.github.com>
2023-08-02 23:23:07 +0300

    Add redshift config and script to auto-switch between light/dark theme based on sun position

---
+++ home/.config/redshift/hooks/theme-switcher.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Note: make sure to enable AppArmor to read this script:
+# https://github.com/jonls/redshift/issues/850#issuecomment-1430106510
+#
+# Location of AppArmor file: /etc/apparmor.d/usr.bin.redshift
+# Add the following between the braces:
+#  owner @{HOME}/.config/redshift.conf r,
+#  owner @{HOME}/.config/redshift/** r,
+#  owner @{HOME}/.config/redshift/hooks/** ux,
+#
+# Related issues:
+# https://github.com/jonls/redshift/pull/149
+# https://github.com/jonls/redshift/issues/858#issuecomment-1125324576
+# https://github.com/jonls/redshift/issues/838
+
+
+# Debug input params of this script:
+# echo "Redshift params: \n $@" >> ~/Desktop/redshift-debug-params.txt
+
+if [ "$1" = "period-changed" ]; then
+  case "$3" in
+    night)
+      xfconf-query -c xsettings -p /Net/ThemeName -s Greybird-dark
+      xfconf-query -c xfwm4 -p /general/theme -s Greybird-dark
+      ;;
+    # transition)
+    #   # Do someting during transition...
+    #   ;;
+    daytime)
+      xfconf-query -c xsettings -p /Net/ThemeName -s Greybird
+      xfconf-query -c xfwm4 -p /general/theme -s Greybird
+      ;;
+  esac
+fi
